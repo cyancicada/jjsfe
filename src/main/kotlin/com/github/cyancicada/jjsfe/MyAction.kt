8d325cff040cc83d4880c376130078f41b633f95
GitHub Action <action@github.com>
2023-08-04 10:23:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/cyancicada/jjsfe/MyAction.kt
+++ src/main/kotlin/com/github/cyancicada/jjsfe/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cyancicada.jjsfe
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
